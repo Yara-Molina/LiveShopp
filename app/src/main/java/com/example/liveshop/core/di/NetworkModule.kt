d212de140417ec9b28811e3f99df8fa1acf26d93
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2026-02-25 20:45:16 -0600

    Fix: inyeccion de dependencias de Network

--- app/src/main/java/com/example/liveshop/core/di/NetworkModule.kt
+++ app/src/main/java/com/example/liveshop/core/di/NetworkModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -11,12 +12,21 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
     @Provides
     @Singleton
     @ProductsRetrofit
-    fun provideProductsRetrofit(): Retrofit {
+    fun provideApiRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://IP/products")
+            .client(okHttpClient)
+            .baseUrl("https://liveshop.duckdns.org")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
